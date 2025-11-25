e7195a4fcc6eef57b37df2f21f7ec64142e91264
1uzzZzzZ <clarita.garciaa@uanl.edu.mx>
2025-11-25 03:11:50 -0600

    semana 1

---
+++ semana 1.py
@@ -0,0 +1,9 @@
+# Declaración de variables
+entero = 10
+flotante = 5.5
+texto = "Análisis de Datos"
+
+# Operación aritmética
+resultado = entero * flotante
+print(f"El resultado de la multiplicación es: {resultado}")
+print(f"La variable de texto es: {texto}")
